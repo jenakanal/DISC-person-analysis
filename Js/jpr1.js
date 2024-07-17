9ba5167f848e64ec8133bd838241665be1a739f9
gena-canal <genacanal@gmail.com>
2024-07-17 14:29:40 +0300

    project

---
+++ Js/jpr1.js
@@ -0,0 +1,282 @@
+var natg1
+var natg2
+var natg3
+var natg4
+var natg5
+var natg6
+var natg7
+var natg8
+var natg9
+var natg10
+var natg11
+var natg12
+var natg13
+var natg14
+var natg15
+var natg16
+var natg17
+var natg18
+var natg19
+var natg20
+var natg21
+var natg22
+var natg23
+var natg24
+
+var ansrs1 = document.querySelectorAll('.A input')
+for (var i = 0; i < ansrs1.length; i++) {
+  ansrs1[i].addEventListener("click" , function(){
+     natg1 = this.value
+    return(natg1)
+  })
+}
+var ansrs2 = document.querySelectorAll('.B input')
+for (var i = 0; i < ansrs2.length; i++) {
+  ansrs2[i].addEventListener("click" , function(){
+     natg2 = this.value
+    return(natg2)
+  })
+}
+var ansrs3 = document.querySelectorAll('.C input')
+for (var i = 0; i < ansrs3.length; i++) {
+  ansrs3[i].addEventListener("click" , function(){
+     natg3 = this.value
+    return(natg3)
+  })
+}
+
+var ansrs4 = document.querySelectorAll('.D input')
+for (var i = 0; i < ansrs4.length; i++) {
+  ansrs4[i].addEventListener("click" , function(){
+     natg4 = this.value
+    return(natg4)
+  })
+}
+var ansrs5 = document.querySelectorAll('.E input')
+for (var i = 0; i < ansrs5.length; i++) {
+  ansrs5[i].addEventListener("click" , function(){
+     natg5 = this.value
+    return(natg5)
+  })
+}
+
+var ansrs6 = document.querySelectorAll('.F input')
+for (var i = 0; i < ansrs6.length; i++) {
+  ansrs6[i].addEventListener("click" , function(){
+     natg6 = this.value
+    return(natg6)
+  })
+}
+
+var ansrs7 = document.querySelectorAll('.G input')
+for (var i = 0; i < ansrs7.length; i++) {
+  ansrs7[i].addEventListener("click" , function(){
+     natg7 = this.value
+    return(natg7)
+  })
+}
+
+var ansrs8 = document.querySelectorAll('.H input')
+for (var i = 0; i < ansrs8.length; i++) {
+  ansrs8[i].addEventListener("click" , function(){
+     natg8 = this.value
+    return(natg8)
+  })
+}
+
+var ansrs9 = document.querySelectorAll('.A1 input')
+for (var i = 0; i < ansrs9.length; i++) {
+  ansrs9[i].addEventListener("click" , function(){
+     natg9 = this.value
+    return(natg9)
+  })
+}
+var ansrs10 = document.querySelectorAll('.B1 input')
+for (var i = 0; i < ansrs10.length; i++) {
+  ansrs10[i].addEventListener("click" , function(){
+     natg10 = this.value
+    return(natg10)
+  })
+}
+var ansrs11 = document.querySelectorAll('.C1 input')
+for (var i = 0; i < ansrs11.length; i++) {
+  ansrs11[i].addEventListener("click" , function(){
+     natg11 = this.value
+    return(natg11)
+  })
+}
+
+var ansrs12 = document.querySelectorAll('.D1 input')
+for (var i = 0; i < ansrs12.length; i++) {
+  ansrs12[i].addEventListener("click" , function(){
+     natg12 = this.value
+    return(natg12)
+  })
+}
+var ansrs13 = document.querySelectorAll('.E1 input')
+for (var i = 0; i < ansrs13.length; i++) {
+  ansrs13[i].addEventListener("click" , function(){
+     natg13 = this.value
+    return(natg13)
+  })
+}
+
+var ansrs14 = document.querySelectorAll('.F1 input')
+for (var i = 0; i < ansrs14.length; i++) {
+  ansrs14[i].addEventListener("click" , function(){
+     natg14 = this.value
+    return(natg14)
+  })
+}
+
+var ansrs15 = document.querySelectorAll('.G1 input')
+for (var i = 0; i < ansrs15.length; i++) {
+  ansrs15[i].addEventListener("click" , function(){
+     natg15 = this.value
+    return(natg15)
+  })
+}
+
+var ansrs16 = document.querySelectorAll('.H1 input')
+for (var i = 0; i < ansrs16.length; i++) {
+  ansrs16[i].addEventListener("click" , function(){
+     natg16 = this.value
+    return(natg16)
+  })
+}
+
+var ansrs17 = document.querySelectorAll('.A2 input')
+for (var i = 0; i < ansrs17.length; i++) {
+  ansrs17[i].addEventListener("click" , function(){
+     natg17 = this.value
+    return(natg17)
+  })
+}
+var ansrs18 = document.querySelectorAll('.B2 input')
+for (var i = 0; i < ansrs18.length; i++) {
+  ansrs18[i].addEventListener("click" , function(){
+     natg18 = this.value
+    return(natg18)
+  })
+}
+var ansrs19 = document.querySelectorAll('.C2 input')
+for (var i = 0; i < ansrs19.length; i++) {
+  ansrs19[i].addEventListener("click" , function(){
+     natg19 = this.value
+    return(natg19)
+  })
+}
+
+var ansrs20 = document.querySelectorAll('.D2 input')
+for (var i = 0; i < ansrs20.length; i++) {
+  ansrs20[i].addEventListener("click" , function(){
+     natg20 = this.value
+    return(natg20)
+  })
+}
+var ansrs21 = document.querySelectorAll('.E2 input')
+for (var i = 0; i < ansrs21.length; i++) {
+  ansrs21[i].addEventListener("click" , function(){
+     natg21 = this.value
+    return(natg21)
+  })
+}
+
+var ansrs22 = document.querySelectorAll('.F2 input')
+for (var i = 0; i < ansrs22.length; i++) {
+  ansrs22[i].addEventListener("click" , function(){
+     natg22 = this.value
+    return(natg22)
+  })
+}
+
+var ansrs23 = document.querySelectorAll('.G2 input')
+for (var i = 0; i < ansrs23.length; i++) {
+  ansrs23[i].addEventListener("click" , function(){
+     natg23 = this.value
+    return(natg23)
+  })
+}
+
+var ansrs24 = document.querySelectorAll('.H2 input')
+for (var i = 0; i < ansrs24.length; i++) {
+  ansrs24[i].addEventListener("click" , function(){
+     natg24 = this.value
+    return(natg24)
+  })
+}
+var natega = document.querySelector('button')
+natega.addEventListener("click" , function(){
+  var taly = [natg1,natg2,natg3,natg4,natg5,natg6,natg7,natg8,natg9,natg10,natg11,natg12,natg13,natg14,natg15,natg16,natg17,natg18,natg19,natg20,natg21,natg22,natg23,natg24]
+  console.log(taly);
+  function checkD(d) {
+if (d=="D"){ return true; }
+}
+var talyD = taly.filter(checkD);
+var contD =talyD.length
+console.log(contD);
+
+function checkII(e) {
+if (e=="I"){ return true; }
+}
+var talyII = taly.filter(checkII);
+var contI =talyII.length
+console.log(contI);
+
+
+function checkS(s) {
+if (s=="S"){ return true; }
+}
+var talyS = taly.filter(checkS);
+var contS =talyS.length
+console.log(contS);
+
+function checkC(c) {
+if (c=="C"){ return true; }
+}
+var talyC = taly.filter(checkC);
+var contC =talyC.length
+console.log(contC);
+var fixd = document.querySelector('.all')
+var dinmc = document.querySelector('.reslt')
+
+var prsn1 =document.querySelectorAll('a')
+fixd.classList.replace("all","hd")
+dinmc.classList.replace("reslt","dsply")
+prsn1[0].innerHTML+= contD
+prsn1[1].innerHTML+= contI
+prsn1[2].innerHTML+= contS
+prsn1[3].innerHTML+= contC
+if(contC==contI&&contC==contS&&contC==contD){
+var prsn = document.querySelector('h5')
+prsn[0].innerHTML="انت شخص متزن"
+}
+else {
+   var x = [contD,contI,contS,contC]
+    var personmax = Math.max(x[0],x[1],x[2],x[3])
+ for (var i = 0; i < x.length; i++) {
+   if(x[i]==personmax){
+     var x2 =["D اى شخص قيادى","I اى شخص اجتماعى","S اى شخص ودود","C اى شخص مدقق"]
+     var y = x.indexOf(x[i])
+     var prsn = document.querySelectorAll('h5')
+     prsn[0].innerHTML= "اول انطباع عند انك شخص " + x2[y]
+    x[i]=0
+     var personmax2 = Math.max(x[0],x[1],x[2],x[3])
+     console.log(x);
+     console.log(personmax2);
+  for (var a = 0; a < x.length; a++) {
+   if(x[a]==personmax2){
+     var y1 = x.indexOf(x[a])
+     var prs = document.querySelectorAll('h6')
+     console.log(x2[y1]);
+     prs[0].innerHTML="ثانى صفة عنك انك شخص " + x2[y1]
+
+   }
+   }
+}
+ }
+}
+
+
+
+  })
